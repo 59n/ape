b9d185816046963e981948e3ef27e3a93da8b5f7
polo <51265624+59n@users.noreply.github.com>
2022-03-07 19:49:47 +0100

    new layout and mobile support

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,158 @@
+$(document.body).ready(function() {
+	
+	// toggle navigation
+	$('.nav-toggle').click(function() {
+		$('.inner').toggleClass('open');
+	});
 
-class TextScramble {
-  constructor(el) {
-    this.el = el;
-    this.chars = '!<>-_\\/[]{}â€”=+*^?#________';
-    this.update = this.update.bind(this);
-  }
-  setText(newText) {
-    const oldText = this.el.innerText;
-    const length = Math.max(oldText.length, newText.length);
-    const promise = new Promise(resolve => this.resolve = resolve);
-    this.queue = [];
-    for (let i = 0; i < length; i++) {
-      const from = oldText[i] || '';
-      const to = newText[i] || '';
-      const start = Math.floor(Math.random() * 40);
-      const end = start + Math.floor(Math.random() * 40);
-      this.queue.push({ from, to, start, end });
-    }
-    cancelAnimationFrame(this.frameRequest);
-    this.frame = 0;
-    this.update();
-    return promise;
-  }
-  update() {
-    let output = '';
-    let complete = 0;
-    for (let i = 0, n = this.queue.length; i < n; i++) {
-      let { from, to, start, end, char } = this.queue[i];
-      if (this.frame >= end) {
-        complete++;
-        output += to;
-      } else if (this.frame >= start) {
-        if (!char || Math.random() < 0.28) {
-          char = this.randomChar();
-          this.queue[i].char = char;
-        }
-        output += `<span class="dud">${char}</span>`;
-      } else {
-        output += from;
-      }
-    }
-    this.el.innerHTML = output;
-    if (complete === this.queue.length) {
-      this.resolve();
-    } else {
-      this.frameRequest = requestAnimationFrame(this.update);
-      this.frame++;
-    }
-  }
-  randomChar() {
-    return this.chars[Math.floor(Math.random() * this.chars.length)];
-  }}
+	// smooth scrolling
+	$(function() {
+		$('a[href*="#"]:not([href="#"])').click(function() {
+			if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				if (target.length) {
+					setTimeout(function() {
+						$('html, body').animate({
+						  scrollTop: target.offset().top
+					  }, 1000);
+					}, 600);
+					return false;
+				}
+			}
+		});
+	});
 
+	var hi = $('.titanic'),
+		header = $('header'),
+		underline = $('.underline'),
+		portfolioBox = $('.portfolio-sec'),
+		project = $('.project'),
+		navLink = $('.nav-link'),
+		skillsBox = $('.skills-sec'),
+		skills = $('.skill'),
+		arrow = $('#arrow'),
+		w = $(window),
+		fz;
 
-const phrases = [
-'Customer Support',
-'Marketing',
-'Advertising',
-'E-commerce',
-'Telegram & Discord Moderation',
-'Coding',
-'IT & Network Integrations'];
+	portfolioBox.css({
+		'marginBottom': $('footer').height()
+	})
 
+	TweenMax.from(hi, 5, {
+		y: 140,
+		opacity: 0,
+		ease: Elastic.easeOut
+	})
+	
+	$('.nav-link').click(function() {
+		$('.inner').toggleClass('open');
+	});
 
-const el = document.querySelector('.text');
-const fx = new TextScramble(el);
+	$('nav li')
+		.mouseenter(function(e) {
+			var underline = $(this).find('div.underline'),
+					width = $(this).find('span').width();
+			TweenMax.to(underline, .1, {
+				width: width
+			});
+		})
+		.mouseleave(function(e) {
+			var underline = $(this).find('div.underline');
+			TweenMax.to(underline, .1, {
+				width: 0
+			});
+		})
 
-let counter = 0;
-const next = () => {
-  fx.setText(phrases[counter]).then(() => {
-    setTimeout(next, 800);
-  });
-  counter = (counter + 1) % phrases.length;
-};
+	w.scroll(function(e) {
+		var wScroll = $(this).scrollTop(),
+			wHeight = $(this).height();
+		if (wScroll >= (header.height() - hi.height()) / 2) {
+			
+			// hi.css({
+			// 	'position': 'absolute',
+			// 	'top': '100%',
+			// 	'transform': 'translate(-50%, -100%)'
+			// })
+			// TweenMax.to(hi, .5, {
+			// 	color: '#333'
+			// })
+			
+			// TweenMax.to(header, .5, {
+			// 	backgroundColor: '#fff'
+			// })
 
-next();
+		}
+		if (wScroll < (header.height() - hi.height()) / 2) {
+			// hi.css({
+			// 	'position': 'fixed',
+			// 	'top': '50%',
+			// 	'transform': 'translate(-50%, -50%)'
+			// })
+			// TweenMax.to(hi, .5, {
+			// 	color: '#fff'
+			// })
+			
+		
+			// TweenMax.to(header, .5, {
+			// 	backgroundColor: '#333'
+			// })
+		}
+	})
+
+	w.resize(function() {
+		portfolioBox.css({
+			'marginBottom': $('footer').height()
+		})
+	})
+
+	$('#portfolio').waypoint(function() {
+		// $('footer').css({
+		// 	'display': 'block'
+		// })
+		TweenMax.staggerTo(project, .25, {
+			'opacity': 1,
+			x: 0
+		}, .25)
+	}, {
+		offset: 300
+	})
+
+	
+	
+	portfolioBox.waypoint(function() {
+		
+		
+	})
+
+	skillsBox.waypoint(function() {
+		skills.filter('.toR').css({
+			'opacity': 1,
+			'transform': 'translateX(0)'
+		});
+		skills.filter('.toL').css({
+			'opacity': 1,
+			'transform': 'translateX(0)'
+		});
+		skills.eq(1).css({
+			'opacity': 1,
+			'transform': 'translateY(0)'
+		});
+		skills.eq(4).css({
+			'opacity': 1
+		});
+		skills.eq(7).css({
+			'opacity': 1,
+			'transform': 'translateY(0)'
+		});
+		
+
+	})
+	
+	arrow.click(function() {
+		$('body').animate({
+			scrollTop: 0
+		}, 1000);
+	});
+
+});
